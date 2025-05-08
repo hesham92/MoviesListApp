41c1084d1bf4ce4023edef27225faf100bcf8f01
hesham <hesham.mohammed@tempo.fit>
2025-05-08 11:33:43 +0300

    Initial Commit

---
+++ MoviesListApp/MoviesListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesListAppApp.swift
+//  MoviesListApp
+//
+//  Created by Hesham on 08/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoviesListAppTests/MoviesListAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesListAppTests.swift
+//  MoviesListAppTests
+//
+//  Created by Hesham on 08/05/2025.
+//
+
+import Testing
+@testable import MoviesListApp
+
+struct MoviesListAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
